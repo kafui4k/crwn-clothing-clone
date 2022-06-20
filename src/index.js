3df09ca3fd62bcdf3d3c0a0ef9094de849c29fed
Kafui Alordo <kalordo7@gmail.com>
2022-06-20 04:27:37 +0000

    moved our directory and shop data into its own reducer, created respective selectors and update corresponding components with ux flow for directory/shop

--- src/index.js
+++ src/index.js
@@ -2,17 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 
 import App from './App';
-import store from './redux/store';
+import {store, persistor} from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
         <BrowserRouter>
-            <React.StrictMode>
+            <PersistGate persistor={persistor}>
                 <App />
-            </React.StrictMode>
+            </PersistGate>
         </BrowserRouter>
     </Provider>,
 );
