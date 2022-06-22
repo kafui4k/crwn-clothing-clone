b43221cdb0f555e2dc629b5bb503c6ebaef26023
Kafui Alordo <kalordo7@gmail.com>
2022-06-22 00:39:25 +0000

    implement code that pulls in our shop data from firestore and stores it into the shop reducer from our shop page. Also included withSpinner HOC

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,9 @@ const config = {
     appId: '1:34956986391:web:d056d7bf4ec3e27d98f452',
 };
 
+// initializing firebase app here...
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
     if (!userAuth) return;
 
@@ -38,15 +41,45 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
     return userRef;
 };
 
-// initializing firebase app here...
-firebase.initializeApp(config);
+export const addCollectionAndDocuments = async (
+    collectionKey,
+    objectsToAdd,
+) => {
+    const collectionRef = firestore.collection(collectionKey);
+
+    const batch = firestore.batch();
+
+    objectsToAdd.forEach((obj) => {
+        const newDocRef = collectionRef.doc();
+        batch.set(newDocRef, obj);
+    });
+
+    return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+    const transfromedCollection = collections.docs.map((doc) => {
+        const {title, items} = doc.data();
+
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items,
+        };
+    });
+
+    return transfromedCollection.reduce((accummulator, collection) => {
+        accummulator[collection.title.toLowerCase()] = collection;
+        return accummulator;
+    }, {});
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({prompt: 'select_account'});
-
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
