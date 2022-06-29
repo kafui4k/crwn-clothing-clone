d7a78f04253dad5c9d958b98bca34f21bb7ab169
Kafui Alordo <kalordo7@gmail.com>
2022-06-29 02:35:30 +0000

    implement Error Boundaries and added a error-boundary component in our App project

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {GlobalStyle} from './global.styles';
 
 import Header from './components/header/header.component';
 import Spinner from './components/spinner/spinner.component';
+import ErrorBoundary from './components/error-boundary/error-boundary.component';
 
 import {selectCurrentUser} from './redux/user/user.selector';
 import {checkUserSession} from './redux/user/user.actions';
@@ -28,22 +29,28 @@ const App = ({checkUserSession, currentUser}) => {
             <GlobalStyle />
             <Header />
             <Switch>
-                <Suspense fallback={<Spinner />}>
-                    <Route exact path='/' component={Homepage} />
-                    <Route path='/shop' component={ShopPage} />
-                    <Route exact path='/checkout' component={CheckoutPage} />
-                    <Route
-                        exact
-                        path='/signin'
-                        render={() =>
-                            currentUser ? (
-                                <Redirect to='/' />
-                            ) : (
-                                <SignInAndSignUp />
-                            )
-                        }
-                    />
-                </Suspense>
+                <ErrorBoundary>
+                    <Suspense fallback={<Spinner />}>
+                        <Route exact path='/' component={Homepage} />
+                        <Route path='/shop' component={ShopPage} />
+                        <Route
+                            exact
+                            path='/checkout'
+                            component={CheckoutPage}
+                        />
+                        <Route
+                            exact
+                            path='/signin'
+                            render={() =>
+                                currentUser ? (
+                                    <Redirect to='/' />
+                                ) : (
+                                    <SignInAndSignUp />
+                                )
+                            }
+                        />
+                    </Suspense>
+                </ErrorBoundary>
             </Switch>
         </div>
     );
